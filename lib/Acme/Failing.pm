b7cf495ac12ae2e761f882d35306c7e1520ba1a0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-08-17 10:45:49 +0900

    initial import

---
+++ lib/Acme/Failing.pm
@@ -0,0 +1,39 @@
+package Acme::Failing;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Acme::Failing -
+
+=head1 SYNOPSIS
+
+  use Acme::Failing;
+
+=head1 DESCRIPTION
+
+Acme::Failing is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
